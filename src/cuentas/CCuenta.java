e0aef260a8197499a6ec9d8a23660803919fd561
richard <richardnavarro@paucasesnovescifp.cat>
2023-02-21 16:03:32 +0100

    Se añade documentación JavaDoc a todo el proyecto

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,98 +1,134 @@
 package cuentas;
 
+/**
+ * Clase CCuenta que es instanciada desde la clase principal Main, con su método
+ * constructor por defecto (sin parámetros) y otro con todos sus atributos.
+ * Además, cuenta con los métodos getter y setter así como distintos métodos
+ * para realizar ingresos y retiradas de saldo.
+ *
+ * @author Richard Navarro <richardnavarro@paucasesnovescifp.cat>
+ * @version 2.0 Esta versión incorpora comentarios de Javadoc
+ * @since 1.5
+ */
 public class CCuenta {
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the cuenta
-     */
-    public String getCuenta() {
-        return cuenta;
-    }
-
-    /**
-     * @param cuenta the cuenta to set
-     */
-    public void setCuenta(String cuenta) {
-        this.cuenta = cuenta;
-    }
-
-    /**
-     * @return the saldo
-     */
-    public double getSaldo() {
-        return saldo;
-    }
-
-    /**
-     * @param saldo the saldo to set
-     */
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
-    /**
-     * @return the tipoInterés
-     */
-    public double getTipoInterés() {
-        return tipoInterés;
-    }
-
-    /**
-     * @param tipoInterés the tipoInterés to set
-     */
-    public void setTipoInterés(double tipoInterés) {
-        this.tipoInterés = tipoInterés;
-    }
-
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    //Métodos constructores
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public double estado()
-    {
+    /**
+     * @return nombre String con el nombre del titular de la cuenta
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre String con el nombre del titular de la cuenta
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return cuenta String con el IBAN identificativo de la cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta String con el IBAN para identificar la cuenta
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return saldo double con el importe del saldo de una cuenta
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo double con el saldo para establecer
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return tipoInterés double con el tipo de interés a devolver
+     */
+    public double getTipoInterés() {
+        return tipoInterés;
+    }
+
+    /**
+     * @param tipoInterés double con el tipo de interés que se desea establecer
+     */
+    public void setTipoInterés(double tipoInterés) {
+        this.tipoInterés = tipoInterés;
+    }
+
+    /**
+     * Método público estado que es invocado desde la clase principal y que
+     * devuelve el saldo de una cuenta bancaria.
+     *
+     * @return saldo double con el saldo de la cuenta bancaria
+     */
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Método público ingresar que es instanciado desde la clase principal y que
+     * recibe por parámetro un double con una cantidad para ingresar, que es
+     * comprobada para verificar que no es un importe negativo y que establece
+     * el nuevo importe del saldo + la cantidad recibida por parámetro.
+     *
+     * @param cantidad double con el importe a ingresar
+     * @throws Exception Esta excepción es lanzada si la cantidad recibida por
+     * parámetro es negativa
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /**
+     * Método público retirar que es llamada desde la clase principal y que
+     * recibe por parámetro un double con una cantidad para retirar, que es
+     * comprobada para verificar que no es un importe superior a 0 y que
+     * establece el nuevo importe del saldo - la cantidad recibida por
+     * parámetro.
+     *
+     * @param cantidad double con el importe a retirar
+     * @throws Exception Esta excepción es lanzada si la cantidad recibida por
+     * parámetro es menor o igual a 0 o si el saldo de la cuenta es inferior al
+     * importe a retirar
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 }
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,11 +1,25 @@
 package cuentas;
 
+/**
+ * Este programa se ha realizado para la práctica de la unidad y simula
+ * a un gestor de cuentas bancarias, donde se realizan ingresos y retiradas
+ * y se pueden realizar consultas de saldos.
+ * @author Richard Navarro <richardnavarro@paucasesnovescifp.cat>
+ * @version 2.0 Esta versión incorpora comentarios de Javadoc
+ * @since 1.5
+ */
+
 public class Main {
 
     public static void main(String[] args) {
         operativa_cuenta(1000);
     }
-
+/**
+ * Método estático que recibe por parámetro un float con una cantidad y que realiza
+ * varias operaciones sobre la clase CCuenta.
+ * @param cantidad Este parámetro de tipo float no tiene funcionalidad, únicamente
+ * se ha creado a modo de ejemplo para practicar en la tarea.
+ */
     public static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
